f8a8795b771af4a78e39e14c43b2544def40c1b6
David Wiman <davwi279@student.liu.se>
2022-04-19 11:09:08 +0200

    initial draft of MapNode class

---
+++ src/map_node.cpp
@@ -0,0 +1,14 @@
+#include "map_node.h"
+
+#include <iostream>
+
+using namespace std;
+
+MapNode::MapNode(string name, MapNode *next_left, MapNode *next_right, unsigned int weight)
+: name{name}, next_left{next_left}, next_right{next_right}, weight{weight} {}
+
+MapNode::~MapNode() {
+    cout << "destroying " << name << endl;
+    delete next_left;
+    delete next_right;
+}
---
+++ src/map_node.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class MapNode {
+public:
+    MapNode(std::string name, MapNode *next_left, MapNode *next_right, unsigned int weight = __INT_MAX__);
+    ~MapNode();
+    void set_weight(int w) {
+        weight = w;
+    }
+    unsigned int get_weight() {
+        return weight;
+    }
+    MapNode* get_next_left() {
+        return next_left;
+    }
+     MapNode* get_next_right() {
+        return next_right;
+    }
+
+
+private:
+    std::string name;
+    unsigned int weight;
+    MapNode *next_left;
+    MapNode *next_right;
+
+};
