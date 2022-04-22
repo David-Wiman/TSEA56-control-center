39026177b0554368f757d96af7e1957c7ad05dbe
David Wiman <davwi279@student.liu.se>
2022-04-22 12:09:05 +0200

    Changed test to accept new constructor in MapGraph

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,33 +8,45 @@ using namespace std;
 
 TEST_CASE("Map Node") {
     SECTION("Constructors") {
-        MapNode node1{"1", nullptr, nullptr};
-        MapNode node2{"2", &node1, nullptr, 34};
-        MapNode node3{"3", &node1, &node2};
+        MapNode node1{"1"};
+        MapNode node2{"2", 34};
+        node2.set_left(2, &node1);
+        MapNode node3{"3"};
+        node3.set_left(2, &node1);
+        node3.set_right(3, &node2);
     }
     SECTION("Constructors (pointers)") {
-        MapNode *node1 = new MapNode{"1", nullptr, nullptr};
-        MapNode *node2 = new MapNode{"2", node1, nullptr, 34};
-        MapNode *node3 = new MapNode{"3", node1, node2};
+        MapNode *node1 = new MapNode{"1"};
+        MapNode *node2 = new MapNode{"2", 34};
+        node2->set_left(2, node1);
+        MapNode *node3 = new MapNode{"3"};
+        node3->set_left(2, node1);
+        node3->set_right(3, node2);
         delete node1;
         delete node2;
         delete node3;
     }
     SECTION("Getters and setters") {
-        MapNode node1{"1", nullptr, nullptr};
-        MapNode node2{"2", &node1, nullptr, 34};
-        MapNode node3{"3", &node1, &node2};
+        MapNode node1{"1"};
+        MapNode node2{"2", 34};
+        node2.set_left(2, &node1);
+        MapNode node3{"3"};
+        node3.set_left(2, &node1);
+        node3.set_right(3, &node2);
         CHECK(node1.get_weight() > 100000);
         CHECK(node2.get_weight() == 34);
         node1.set_weight(8);
         CHECK(node1.get_weight() == 8);
     }
     SECTION("Get next") {
-        MapNode node1{"1", nullptr, nullptr};
-        MapNode node2{"2", &node1, nullptr, 34};
-        MapNode node3{"3", &node1, &node2};
+        MapNode node1{"1"};
+        MapNode node2{"2", 34};
+        node2.set_left(2, &node1);
+        MapNode node3{"3"};
+        node3.set_left(2, &node1);
+        node3.set_right(3, &node2);
 
-        CHECK(*node2.get_next_left() == node1);
+        CHECK(*node2.get_left().node == node1);
         //CHECK(node2.get_next_right() != node1);
     }
 }
